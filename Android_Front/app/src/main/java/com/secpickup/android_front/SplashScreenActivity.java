4fc3950aa7e4c3f9488468ae2b5377da5710e957
ZakaeiaeSAIDI <zakariae.saidi@uit.ac.ma>
2023-11-22 12:03:22 +0100

    Create the User Login Interface and updating the Colors ressource

--- Android_Front/app/src/main/java/com/secpickup/android_front/SplashScreenActivity.java
+++ Android_Front/app/src/main/java/com/secpickup/android_front/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.secpickup.android_front;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //redirection vers main activity apres 3 secondes
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                //demarrer une page
+                Intent intent= new Intent(getApplicationContext(), activity_login.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        // handler post delayed
+        new Handler().postAtTime(runnable, 3000);
     }
 }
