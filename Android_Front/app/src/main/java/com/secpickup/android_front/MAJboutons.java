73fadbf7574dfd06b56dca91d639e839ad274f22
MedKHCHINE <mohamed.elkhchine1@uit.ac.ma>
2023-12-22 16:17:46 +0100

    MAJ buttons

---
+++ Android_Front/app/src/main/java/com/secpickup/android_front/MAJboutons.java
@@ -0,0 +1,53 @@
+package com.secpickup.android_front;
+
+import android.content.Context;
+import android.widget.Button;
+import androidx.core.content.ContextCompat;
+import com.secpickup.android_front.R;
+
+public class MAJboutons {
+
+    private final Context context;
+
+    public MAJboutons(Context context) {
+        this.context = context;
+    }
+
+    public void updateButtonColors(Button btnRecuperer, Button btnDeposer, Button btnAbsent, String situation) {
+        // Réinitialisez toutes les couleurs à leur état d'origine
+        resetButtonColors(btnRecuperer, btnDeposer, btnAbsent);
+
+        // Changez la couleur et l'état activé des boutons en fonction de la situation
+        if ("deposed".equals(situation)) {
+            setButtonDisabledAndGrey(btnDeposer);
+            setButtonEnabledAndGreen(btnRecuperer, btnAbsent);
+        } else if ("recuperated".equals(situation)) {
+            setButtonDisabledAndGrey(btnRecuperer);
+            setButtonEnabledAndGreen(btnDeposer, btnAbsent);
+        } else if ("absent".equals(situation)) {
+            setButtonDisabledAndGrey(btnAbsent);
+            setButtonEnabledAndGreen(btnDeposer, btnRecuperer);
+        }
+    }
+
+    private void resetButtonColors(Button... buttons) {
+        for (Button button : buttons) {
+            button.setEnabled(true);
+            button.setBackgroundColor(ContextCompat.getColor(context, R.color.teal_200));
+        }
+    }
+
+    private void setButtonEnabledAndGreen(Button... buttons) {
+        for (Button button : buttons) {
+            button.setEnabled(true);
+            button.setBackgroundColor(ContextCompat.getColor(context, R.color.teal_200));
+        }
+    }
+
+    private void setButtonDisabledAndGrey(Button... buttons) {
+        for (Button button : buttons) {
+            button.setEnabled(false);
+            button.setBackgroundColor(ContextCompat.getColor(context, R.color.grey));
+        }
+    }
+}
