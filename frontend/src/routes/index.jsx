0a3997a4d3f312aec4fbed396af22a604b1be7e3
Omar-Kerchaoui <omarker2002@gmail.com>
2023-12-24 10:34:50 +0100

    add assistante pages

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import AppLayout from "../components/layout/AppLayout";
 import AuthLayout from "../components/layout/AuthLayout";
 import ErrorPage from "../error-page";
+import { assistantesRoutes } from "./assistantes";
 import { authRoutes } from "./auth";
 import { parentRoutes } from "./parents";
 
@@ -11,7 +12,7 @@ export const mainRoutes = [
       {
         path: "/",
         element: <AppLayout />,
-        children: [...parentRoutes],
+        children: [...parentRoutes,...assistantesRoutes],
       },
       {
         path: "/auth",
