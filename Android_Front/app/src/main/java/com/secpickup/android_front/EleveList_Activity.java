d676ebf60babd0bcaeca14eeb2fa00094ffe6410
HMOUDAT OUSSAMA <hmoudat.oussama@gmail.com>
2023-12-24 11:25:31 +0100

    adding lateral bar / notification / Parametres Button

--- Android_Front/app/src/main/java/com/secpickup/android_front/EleveList_Activity.java
+++ Android_Front/app/src/main/java/com/secpickup/android_front/EleveList_Activity.java
@@ -1,23 +1,23 @@
 package com.secpickup.android_front;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.secpickup.android_front.Adapter.EleveAdapter;
 import com.secpickup.android_front.modele.Eleve;
-import com.secpickup.android_front.modele.Parent;
 import com.secpickup.android_front.modele.UserAccountType;
 import com.secpickup.android_front.retrofit.EleveApi;
 import com.secpickup.android_front.retrofit.RetrofitService;
-import com.secpickup.android_front.retrofit.UserApi;
-
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -33,7 +33,31 @@ public class EleveList_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_eleve_list);
+        setContentView(R.layout.activity_main_parent);
+
+        MaterialToolbar toolbar = findViewById(R.id.toolBar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(EleveList_Activity.this, "Vous avez cliqué sur le menu de navigation", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.menuSearch) {
+                    Toast.makeText(EleveList_Activity.this, "Vous avez cliqué sur le menu recherche", Toast.LENGTH_SHORT).show();
+                    return true;
+                } else if (item.getItemId() == R.id.menuSettings) {
+                    Toast.makeText(EleveList_Activity.this, "Vous avez cliqué sur le menu paramètre", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                return false;
+            }
+        });
 
         Intent intent = getIntent();
         if (intent.hasExtra("username") && intent.hasExtra("type")) {
