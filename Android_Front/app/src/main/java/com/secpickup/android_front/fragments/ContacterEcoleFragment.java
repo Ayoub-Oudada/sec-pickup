bb0d310924538cee1adbab5fe01d5e71cdfd86f8
HMOUDAT OUSSAMA <hmoudat.oussama@gmail.com>
2023-12-27 19:37:10 +0100

    adding contact ecole popup

--- Android_Front/app/src/main/java/com/secpickup/android_front/fragments/ContacterEcoleFragment.java
+++ Android_Front/app/src/main/java/com/secpickup/android_front/fragments/ContacterEcoleFragment.java
@@ -10,11 +10,6 @@ import android.view.ViewGroup;
 
 import com.secpickup.android_front.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContacterEcoleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ContacterEcoleFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -30,15 +25,7 @@ public class ContacterEcoleFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContacterEcoleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static ContacterEcoleFragment newInstance(String param1, String param2) {
         ContacterEcoleFragment fragment = new ContacterEcoleFragment();
         Bundle args = new Bundle();
