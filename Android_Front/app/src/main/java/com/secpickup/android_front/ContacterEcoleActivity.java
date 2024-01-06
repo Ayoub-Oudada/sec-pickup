5c0d71e49a09dee6ffa082d743cd92656bc81145
Louzaoui Safaa <safaaLouzaoui@gmail.com>
2024-01-06 22:41:05 +0100

    contacterEcole

---
+++ Android_Front/app/src/main/java/com/secpickup/android_front/ContacterEcoleActivity.java
@@ -0,0 +1,14 @@
+package com.secpickup.android_front;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContacterEcoleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacter_ecole);
+    }
+}
