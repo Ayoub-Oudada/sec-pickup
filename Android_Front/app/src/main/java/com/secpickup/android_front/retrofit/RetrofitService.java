a1fa0c71afeca8f5db8454dd2037e47281fe498d
Louzaoui Safaa <safaaLouzaoui@gmail.com>
2024-01-12 21:59:08 +0100

    update list_enfants

--- Android_Front/app/src/main/java/com/secpickup/android_front/retrofit/RetrofitService.java
+++ Android_Front/app/src/main/java/com/secpickup/android_front/retrofit/RetrofitService.java
@@ -20,7 +20,7 @@ public class RetrofitService {
                 .setLenient()
                 .create();
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.1.7:8080/")
+                .baseUrl("http://172.17.223.81:8080/")
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build();
     }
